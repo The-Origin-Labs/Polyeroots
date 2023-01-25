86202ca90cca21e396f4f5a1abbf5a4541eea1b2
Siddhant Prateek <siddhantprateek@gmail.com>
2023-01-25 15:35:46 +0530

    api geteway endpoint handlers

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -1,11 +1,27 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+)
 
 func ApiHandler() {
 
-	route := fiber.New()
+	route := fiber.New(fiber.Config{
+		JSONEncoder: json.Marshal,
+		JSONDecoder: json.Unmarshal,
+	})
 
+	// Middleware
+	route.Use(cors.New())
+
+	// API metrics endpoint
+	route.Get("/metrics", monitor.New(monitor.Config{Title: "API Gateway Monitoring Dashboard"}))
+
+	// Endpoints
 	route.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Server is Healthy✅")
 	})
